d027b2ee9542e4ae75026a9942cde2462d92055e
Fahim <fahimshahariar.fs@gmail.com>
2025-06-15 19:08:19 +0600

    Add project files.

---
+++ OOP/Classes/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP.Classes
+{
+    internal class Person
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Greet()
+        {
+
+            Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+
+            Console.WriteLine();
+
+        }
+
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using OOP.Classes;
+using System;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person intro = new Person(name: "Fahim", age: 25);
+
+            intro.Greet();
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
