aa457d5c325a997acd939a46d915d776275dff97
Fahim <fahimshahariar.fs@gmail.com>
2025-06-15 18:50:58 +0600

    Nothing

---
+++ OOP/Classes/Book.cs
@@ -0,0 +1,26 @@
+﻿namespace OOP.Classes
+{
+    internal class Book
+    {
+
+        // Properties of the Book class
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+
+        // Constructor to initialize a new instance of the Book class
+        public Book(string title, string author, int publicationYear)
+        {
+            Title = title;
+            Author = author;
+            PublicationYear = publicationYear;
+        }
+
+        // Method to display book information
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Title: {Title}, Author: {Author}, Year: {PublicationYear}");
+        }
+
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Main(string[] args)
+{
+    Console.WriteLine("Hello, World!");
+    // Create an instance of the Book class
+    OOP.Classes.Book myBook = new OOP.Classes.Book(title: "1984", author: "George Orwell", publicationYear: 1949);
+
+
+
+    // You can now use myBook to access properties and methods defined in the Book class
+    // For example, if you had a method called DisplayInfo, you could call it like this:
+    myBook.DisplayInfo();
+}
